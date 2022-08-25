e28aef7953b0cc9c8bd6d57bc2cdf39e09cfb8cc
Nataliya <nataliya.laz@gmail.com>
2022-08-25 16:56:17 +0300

    add disappear animation

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -9,44 +9,91 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var deck = PlayingCardDeck()
-
-    @IBOutlet weak var playingCardView: PlayingCardView!{
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.left,.right]
-            playingCardView.addGestureRecognizer(swipe)
-            
-            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(playingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy: )))
-            playingCardView.addGestureRecognizer(pinch)
-        }
+    private var deck = PlayingCardDeck()
+    
+    @IBOutlet var cardViews: [PlayingCardView]!
+    
+    private var faceUpCardsViews: [PlayingCardView] {
+        cardViews.filter{ $0.isFaceUp && !$0.isHidden }
     }
     
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
-        case .ended: playingCardView.isFaceUp = !playingCardView.isFaceUp
-        default: break
-        }
+    private var faceUpCardViewsMatch: Bool {
+        return faceUpCardsViews.count == 2 && faceUpCardsViews[0].rank == faceUpCardsViews[1].rank && faceUpCardsViews[0].suit == faceUpCardsViews[1].suit
     }
-    
-    @objc func nextCard() {
-        if let card = deck.draw() {
-            playingCardView.rank = card.rank.order
-            playingCardView.suit = card.suit.rawValue
-        }
-    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for _ in 1...10 {
-            if let card = deck.draw(){
-                print("\(card)")
-            }
+        var cards = [PlayingCard]()
+        
+        for _ in 1...((cardViews.count + 1)/2) {
+            let card = deck.draw()!
+            cards += [card, card]
+        }
+        for cardView in cardViews {
+            cardView.isFaceUp = false
+            let card = cards.remove(at: cards.count.arc4random)
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+            cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard)))
         }
     }
-
-
+    
+    @objc func flipCard(_ recognizer: UITapGestureRecognizer) {
+        switch recognizer.state {
+        case .ended:
+            if let chosenCardView = recognizer.view as? PlayingCardView {
+                UIView.transition(with: chosenCardView,
+                                  duration: 0.6,
+                                  options: [.transitionFlipFromLeft],
+                                  animations:{
+                    chosenCardView.isFaceUp = !chosenCardView.isFaceUp
+                },
+                                  completion:{ finished in// here we don't have memory cycle because closure does capture self, self doesnt any way point to this closure. Animation system only has pointer to this closure
+                    if self.faceUpCardViewsMatch {
+                        UIViewPropertyAnimator.runningPropertyAnimator(
+                            withDuration: 0.6,
+                            delay: 0,
+                            animations: {
+                                self.faceUpCardsViews.forEach {
+                                    $0.transform = CGAffineTransform.identity.scaledBy(x: 2.0, y: 2.0)
+                                }
+                            },
+                            completion: { position in
+                                UIViewPropertyAnimator.runningPropertyAnimator(
+                                    withDuration: 0.75,
+                                    delay: 0,
+                                    animations: {
+                                        self.faceUpCardsViews.forEach {
+                                            $0.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
+                                            $0.alpha = 0
+                                        }
+                                    },
+                                    completion: { position in
+                                        self.faceUpCardsViews.forEach {
+                                            $0.isHidden = true
+                                            $0.alpha = 1
+                                            $0.transform = .identity
+                                        }
+                                    }
+                                )
+                            }
+                        )
+                    } else if self.faceUpCardsViews.count == 2 {
+                        self.faceUpCardsViews.forEach { cardView in
+                            UIView.transition(with: cardView,
+                                              duration: 0.6,
+                                              options: [.transitionFlipFromLeft],
+                                              animations:{
+                                cardView.isFaceUp = false
+                            })
+                        }
+                    }
+                }
+                )
+            }
+        default: break
+        }
+    }
 }
 
